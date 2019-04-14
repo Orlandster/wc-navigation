5eaef435fa62fb995750b8b708c8db1801b21c41
Orlando Wenzinger <orlando.wenzinger@bluewin.ch>
2019-04-14 10:37:32 +0200

    [Feature]: Add styles architecture

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'wc-navigation',
+  globalStyle: 'src/globals.scss',
   outputTargets:[
     { type: 'dist' },
     { type: 'docs' },
